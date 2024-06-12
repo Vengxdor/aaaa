351eed9ccf2fba112106969a5c771b49c75a149a
Steven Fernandez <fernandezsteven185@gmail.com>
2024-06-12 12:28:17 -0500

    fix: small change

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -30,7 +30,7 @@ export default function Hero () {
       </header>
       <div>
         {/* todo: Add hover and active effect. */}
-        <button className="focus:outline-nonefocus:ring-slate-400 mt-7 inline-flex  h-12 w-52 animate-shimmer items-center justify-center gap-2 rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition duration-300 hover:-translate-y-1  hover:scale-105 focus:ring-offset-slate-50">
+        <button className="focus:outline-nonefocus:ring-slate-400 mt-10 inline-flex  h-12 w-52 animate-shimmer items-center justify-center gap-2 rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition duration-300 hover:-translate-y-1  hover:scale-105 focus:ring-offset-slate-50">
           See my work
           <NavigateIcon />
         </button>
