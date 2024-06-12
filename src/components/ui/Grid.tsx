24739a1d27692cf62ab98fd7cb2f219d86e44b49
Steven Fernandez <fernandezsteven185@gmail.com>
2024-06-12 12:29:00 -0500

    feat: Add contact and skills components.

--- src/components/ui/Grid.tsx
+++ src/components/ui/Grid.tsx
@@ -1,38 +1,72 @@
-/* eslint-disable react/jsx-key */ // todo
+'use client'
 import React from 'react'
 import { BentoGrid, BentoGridItem } from '../BentoGrid'
-import Carousel from './Carousel'
 
 import GridPattern from './GridPattern'
-import { IconCloudDemo } from './Cloud'
+import CloudOfIcons from './Cloud'
+import { EmailIcon, LinkedinIcon, TwitterIcon } from '../icons'
+import { toast } from 'sonner'
+import { Meteors } from '../Meteors'
 
-export default function Grid () {
+const Contact = () => {
+  const handleCopyEmail = async () => {
+    try {
+      toast.success('Email copied.')
+      await navigator.clipboard.writeText('Stevenfernandeztango3@gmail.com')
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  const handleCopy = () => {
+    handleCopyEmail().catch((err) => {
+      console.error(err)
+    })
+  }
   return (
-    <BentoGrid className="mt-20 animate-onload-slow ">
-      {gridItems.map((item, i) => (
-        <BentoGridItem
-          key={i}
-          header={item.header}
-          className={`${i === 1 || i === 2 ? 'md:col-span-2 ' : ''} ${i === 4 && 'md:col-span-3'}`}
-        />
-      ))}
-    </BentoGrid>
+    <>
+      <div className="relative z-10 size-full overflow-hidden p-3 pt-7">
+        <header className="text-center">
+          <h2 className="text-6xl font-bold">Interested?</h2>
+          <p className="text-sm text-white/50">Connect with me here:</p>
+        </header>
+        <div className="mt-10 flex items-center justify-center gap-3 text-6xl">
+          <a
+            className="opacity-80 transition duration-300 hover:scale-105 hover:opacity-100"
+            href="#"
+          >
+            <LinkedinIcon />
+          </a>
+          <button
+            onClick={handleCopy}
+            className="opacity-80 transition duration-300 hover:scale-105 hover:opacity-100"
+          >
+            <EmailIcon />
+          </button>
+          <a
+            className="opacity-80 transition duration-300 hover:scale-105 hover:opacity-100"
+            href="#"
+          >
+            <TwitterIcon />
+          </a>
+        </div>
+      </div>
+      <div className="absolute inset-0 size-full overflow-hidden">
+        <Meteors />
+      </div>
+    </>
   )
 }
-const Skeleton = () => (
-  <div className="flex size-full min-h-24 flex-1 rounded-xl bg-gradient-to-br from-neutral-200 to-neutral-100 dark:from-neutral-900 dark:to-neutral-800"></div>
-)
 const Information = () => (
   <>
     <div className="size-full p-4 px-6">
       <h2 className="my-2 text-3xl font-semibold">Hello! Steven here.</h2>
-      <p className='opacity-90'>
-        I&apos;m a front-end developer based in Chicago, IL. Currently, I freelance
-        for individuals who need visually appealing websites built with best
-        practices.
+      <p className="opacity-90">
+        I&apos;m a front-end developer based in Chicago, IL. Currently, I
+        freelance for individuals who need visually appealing websites built
+        with best practices.
       </p>
       <br />
-      <p className='opacity-90'>
+      <p className="opacity-90">
         I began my journey in 2022, and since then, I have experienced
         exponential growth in my knowledge. Now, I am capable of creating
         excellent user experiences.
@@ -46,27 +80,37 @@ const Information = () => (
 
 const gridItems = [
   {
-    // header: (
-    //   <img
-    //     className="size-full rounded-xl object-cover"
-    //     src="/author.jpg"
-    //     alt=""
-    //   />
-    // )
+    header: (
+      <img
+        className="size-full rounded-xl object-cover"
+        src="/author.jpg"
+        alt=""
+      />
+    )
   },
   {
-    // header: <Information />
+    header: <Information />
   },
   {
-    title: 'The Art of Design',
-    description: 'Discover the beauty of thoughtful and functional design.',
-    header: <Skeleton />
+    header: <Contact />
   },
   {
-    header: <IconCloudDemo />
-  },
-
-  {
-    header: <Carousel />
+    header: <CloudOfIcons />
   }
 ]
+
+export default function Grid () {
+  return (
+    <BentoGrid className="mt-20 animate-onload-slow ">
+      {gridItems.map((item, i) => (
+        <BentoGridItem
+          key={i}
+          header={item.header}
+          className={`${i === 1 || i === 2 ? 'md:col-span-2 ' : ''} ${
+            i === 2 && 'md:row-span-1'
+          }`}
+        />
+      ))}
+    </BentoGrid>
+  )
+}
