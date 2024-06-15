7f4189e21be5e62ace86c1d6ecbff747de138a83
Steven Fernandez <fernandezsteven185@gmail.com>
2024-06-14 21:24:25 -0500

    feat: Add project and footer sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Navbar from '@/components/ui/Navbar'
 import Hero from '@/components/sections/Hero'
 import Grid from '@/components/ui/Grid'
+import Projects from '@/components/sections/Projects'
+import Footer from '@/components/sections/Footer'
 
 export default function Home () {
   return (
@@ -9,7 +11,9 @@ export default function Home () {
       <main className="mx-auto my-10 w-[860px]">
         <Hero />
         <Grid />
+        <Projects />
       </main>
+      <Footer />
     </>
   )
 }
