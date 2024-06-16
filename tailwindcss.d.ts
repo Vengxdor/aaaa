55aa55d32a2d94819fa28abadcea138af51b3f7a
Steven Fernandez <fernandezsteven185@gmail.com>
2024-06-15 19:40:59 -0500

    fix: tailwind module

---
+++ tailwindcss.d.ts
@@ -0,0 +1,5 @@
+declare module 'tailwindcss/lib/util/flattenColorPalette' {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  const flattenColorPalette: Function
+  export default flattenColorPalette
+}
